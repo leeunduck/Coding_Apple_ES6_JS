c0754833e11ea8062dcbe0941f2760c266d6c0b8
HyunseokLEE <unducklee@gmail.com>
2024-06-11 15:40:09 +0900

    feat: global variable practice problem

--- part1/index.js
+++ part1/index.js
@@ -1,30 +1,6 @@
-/** 문제 1번 **/
-let personTest = {
-  name: "손흥민",
-  sayHi: function testFun() {
-    console.log(`안녕 나는 ${this.name}`);
-  },
-};
-personTest.sayHi();
+if (true) {
+  let a = 1;
+  var b = 2;
 
-/** 문제 2번 **/
-let testDocument = {
-  data: [1, 2, 3, 4, 5],
-};
-
-testDocument.allPlus = function () {
-  let plus = 0;
-  this.data.forEach(function (num) {
-    plus = plus + num;
-  });
-  console.log(plus);
-};
-
-testDocument.allPlus();
-
-/** 문제 3번 **/
-document.getElementById("btn").addEventListener("click", function () {
-  setTimeout(() => {
-    console.log(this.innerHTML);
-  }, 1000);
-});
+  console.log(b);
+}
