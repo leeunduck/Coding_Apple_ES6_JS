52da786ccd8e15a3f4951b9cbb97c5d34d998e93
HyunseokLEE <unducklee@gmail.com>
2024-06-26 00:57:08 +0900

    feat: tagged literal problem 2

--- part1/index.js
+++ part1/index.js
@@ -1,9 +1,21 @@
 /** tagged literal 문제 1번 **/
-let pants = 20;
+// let pants = 20;
+// let socks = 100;
+
+// function analyzer(text, variable1, variable2) {
+//   console.log(text[1] + variable2 + text[0] + variable1);
+// }
+
+// analyzer`바지${pants} 양말${socks}`;
+
+/** tagged literal 문제 2번 **/
+let pants = 0;
 let socks = 100;
 
 function analyzer(text, variable1, variable2) {
-  console.log(text[1] + variable2 + text[0] + variable1);
+  if (variable1 == 0) {
+    console.log(text[0] + "다팔렸어요" + text[1] + variable2);
+  }
 }
 
 analyzer`바지${pants} 양말${socks}`;
