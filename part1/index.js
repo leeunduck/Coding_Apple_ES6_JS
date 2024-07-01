ce4976904028e36fb5359188ba2d97d88e899b8f
HyunseokLEE <unducklee@gmail.com>
2024-07-01 21:42:04 +0900

    feat: rest parameter problem

--- part1/index.js
+++ part1/index.js
@@ -1,21 +1,7 @@
-/** tagged literal 문제 1번 **/
-// let pants = 20;
-// let socks = 100;
-
-// function analyzer(text, variable1, variable2) {
-//   console.log(text[1] + variable2 + text[0] + variable1);
-// }
-
-// analyzer`바지${pants} 양말${socks}`;
-
-/** tagged literal 문제 2번 **/
-let pants = 0;
-let socks = 100;
-
-function analyzer(text, variable1, variable2) {
-  if (variable1 == 0) {
-    console.log(text[0] + "다팔렸어요" + text[1] + variable2);
-  }
+/** rest parmeter problem **/
+function arrayTest(...parameters) {
+  console.log(parameters);
 }
 
-analyzer`바지${pants} 양말${socks}`;
+let newArray = arrayTest(1, 2, 3, 4, 5, 6, 7, 8, 9);
+console.log(newArray);
