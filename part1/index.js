47935c242c7748c724107a624af95c5de0cc2c97
HyunseokLEE <unducklee@gmail.com>
2024-07-01 21:42:35 +0900

    feat: max problem

--- part1/index.js
+++ part1/index.js
@@ -5,3 +5,7 @@ function arrayTest(...parameters) {
 
 let newArray = arrayTest(1, 2, 3, 4, 5, 6, 7, 8, 9);
 console.log(newArray);
+
+/** max problem **/
+let numbers = [2, 3, 4, 5, 6, 1, 3, 2, 5, 5, 4, 6, 7];
+console.log(Math.max(...numbers));
