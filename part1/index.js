9ed4dfb4e9d3da385fcd5156f5d98f7f8a0c773b
HyunseokLEE <unducklee@gmail.com>
2024-07-01 21:50:55 +0900

    feat: sort problem

--- part1/index.js
+++ part1/index.js
@@ -9,3 +9,10 @@ console.log(newArray);
 /** max problem **/
 let numbers = [2, 3, 4, 5, 6, 1, 3, 2, 5, 5, 4, 6, 7];
 console.log(Math.max(...numbers));
+
+/** sort problem **/
+function sortTest(word) {
+  console.log([...word].sort());
+}
+
+sortTest("bear");
